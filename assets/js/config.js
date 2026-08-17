e905179197dc3c450bdafc104ff26de37c2a3571
samstallan0707 <samstallan0707@gmail.com>
2026-08-17 23:09:54 +0100

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.LINDSELL_SITE_CONFIG = {
   supabaseUrl: "https://woycxdbgctgkveclnxrf.supabase.co",
   supabasePublishableKey: "sb_publishable_SwhK7bYiPlIMI_yHErpdBA_WMZOSlWO",
-  oldLiveScoreUrl: "https://samstallan0707.github.io/lindsell-live-score/",
+  oldLiveScoreUrl: "live.html",
   playCricketSiteUrl: "https://lindsell.play-cricket.com"
 };
